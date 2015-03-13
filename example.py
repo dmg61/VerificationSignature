dac2095367a86bc0c2aa5543c76ca58bc0994e70
Агазадян Артур <arturag0794@gmail.com>
2015-03-13 23:48:45 +0300

    Add comments Refactoring code

--- example.py
+++ example.py
@@ -9,7 +9,6 @@ import scipy as sp
 import sys
 from matplotlib import pyplot as plt
 
-
 def drawMatches(img1, kp1, img2, kp2, matches):
     """
     My own implementation of cv2.drawMatches as OpenCV 2.4.9
@@ -40,13 +39,13 @@ def drawMatches(img1, kp1, img2, kp2, matches):
     rows2 = img2.shape[0]
     cols2 = img2.shape[1]
 
-    out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
+    out = np.zeros((max([rows1, rows2]), cols1+cols2, 3), dtype='uint8')
 
     # Place the first image to the left
-    out[:rows1,:cols1,:] = np.dstack([img1, img1, img1])
+    out[:rows1, :cols1, :] = np.dstack([img1, img1, img1])
 
     # Place the next image to the right of it
-    out[:rows2,cols1:cols1+cols2,:] = np.dstack([img2, img2, img2])
+    out[:rows2, cols1:cols1+cols2, :] = np.dstack([img2, img2, img2])
 
     # For each pair of points we have between both images
     # draw circles, then connect a line between them
@@ -58,26 +57,34 @@ def drawMatches(img1, kp1, img2, kp2, matches):
 
         # x - columns
         # y - rows
-        (x1,y1) = kp1[img1_idx].pt
-        (x2,y2) = kp2[img2_idx].pt
+        (x1, y1) = kp1[img1_idx].pt
+        (x2, y2) = kp2[img2_idx].pt
 
         # Draw a small circle at both co-ordinates
         # radius 4
         # colour blue
         # thickness = 1
-        cv2.circle(out, (int(x1),int(y1)), 4, (255, 0, 0), 1)
-        cv2.circle(out, (int(x2)+cols1,int(y2)), 4, (255, 0, 0), 1)
+        cv2.circle(out, (int(x1), int(y1)), 4, (255, 0, 0), 1)
+        cv2.circle(out, (int(x2)+cols1, int(y2)), 4, (255, 0, 0), 1)
 
         # Draw a line in between the two points
         # thickness = 1
         # colour blue
-        cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (255, 0, 0), 1)
+        cv2.line(out, (int(x1), int(y1)), (int(x2)+cols1, int(y2)), (255, 0, 0), 1)
 
     # Show the image
     cv2.imshow('Matched Features', out)
     cv2.moveWindow("Matched Features", 0, image1.shape[0])
 
 def search_circles(src): # Count c  ircles in image
+    """
+    Анализ окружностей на изображении
+
+    @param src  : Входное изображение
+
+    @return : Список окружностей
+    """
+
     circles = cv2.HoughCircles(src, cv.CV_HOUGH_GRADIENT, 1, 20,
                                param1=50, param2=30, minRadius=0, maxRadius=0)
     circles = cv2.HoughCircles(src, cv2.HOUGH_GRADIENT, 1.2, 100)
@@ -85,6 +92,14 @@ def search_circles(src): # Count c  ircles in image
     return circles
 
 def search_lines(src): # Count lines in image
+    """
+    Анализ линий на изображении
+
+    @param src  : Входное изображение
+
+    @return : Список параметров анализа
+              (Список линий, средний угол наклона линий)
+    """
 
     #lines = cv2.HoughLines(src, cv.CV_HOUGH_PROBABILISTIC, np.pi/180, 100)
     lines = cv2.HoughLines(src, cv.CV_HOUGH_PROBABILISTIC, np.pi/180, 5, 45, 8)
@@ -102,13 +117,22 @@ def search_lines(src): # Count lines in image
     return lines, averadge_slope_line
 
 def analysis_pictures(img):
+    """
+    Анализ изображения
+
+    @param img  : Входное изображение
+
+    @return : Список параметров анализа
+              (Кол-во линий, кол-во окружнойстей, угол наклона,
+              кол-во элементов, ширина, высота, площадь)
+    """
 
     param = {}
 
     gray  = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
     blur  = cv2.blur(gray, (7, 7))
     gb    = cv2.GaussianBlur(blur, (7, 7), 0)
-    edges = cv2.Canny(gb, 50, 150)
+    #edges = cv2.Canny(gb, 50, 150)
     edges = cv2.Canny(gb, 100, 200)
     #edges = cv2.Canny(gb, 300, 100)
 
@@ -117,27 +141,11 @@ def analysis_pictures(img):
     param['lines']    = lines
     param['averadge'] = averadge
 
-    # for rho, theta in lines[0]:
-    #     a = np.cos(theta)
-    #     b = np.sin(theta)
-    #     x0 = a*rho
-    #     y0 = b*rho
-    #     x1 = int(x0 + 1000 * (-b))
-    #     y1 = int(y0 + 1000 * (a))
-    #     x2 = int(x0 - 1000 * (-b))
-    #     y2 = int(y0 - 1000 * (a))
-
-        #cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
     #circle = cv2.HoughCircles(edges, cv.CV_HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
 
     circle = cv2.HoughCircles(edges, cv.CV_HOUGH_GRADIENT, 5, 25)
 
     circles = np.uint16(np.around(circle))
-
-    #for i in circle[0,:]:
-        # draw the outer circle
-        #cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
 
     ret, thresh  = cv2.threshold(edges, 1, 255, 1)
 
@@ -166,45 +174,50 @@ def analysis_pictures(img):
     param['height'] = tmp['height']
     param['square'] = tmp['square']
 
-    #cnt = contours
-    #rect = cv2.minAreaRect(contours[0])
-
-    #hull = convex_hull(cnt)
-    # rect = cv2.minAreaRect(cnt)
-    # box = cv2.boxPoints(rect)
-    # box = np.int0(box)
-    # img = cv2.drawContours(img,[box],0,(0,0,255),2)
-
     return param
 
 def mapping_img(img):
+    """
+    Обработка изображения (размытие, выделение границ)
+
+    @param img  : Входное изображение
+
+    @return : Обработанное изображение
+    """
 
     blur  = cv2.blur(img.copy(), (7, 7))
     gb    = cv2.GaussianBlur(blur, (7, 7), 0)
-    edges = cv2.Canny(gb, 50, 150)
+    #edges = cv2.Canny(gb, 50, 150)
     edges = cv2.Canny(gb, 100, 200)
 
     return edges
 
 def analis_object_in_img(img):
+    """
+    Поиск и анализ элементов изображение
 
-    #Преобразуем в оттенок серого
-    gray = cv2.cvtColor(img.copy(),cv2.COLOR_BGR2GRAY)
+    @param img  : Входное изображение
 
-    #Вычисляем величину градиента в вертикальном и горизонтальном направлениях
+    @return : Список параметров анализа (Кол-во элементов, ширина, высота, суммарная площадь прямоугольников)
+    """
+
+    # Преобразуем в оттенок серого
+    gray = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
+
+    # Вычисляем величину градиента в вертикальном и горизонтальном направлениях
     gradX = cv2.Sobel(gray, ddepth = cv2.cv.CV_32F, dx = 1, dy = 0, ksize = -1)
     gradY = cv2.Sobel(gray, ddepth = cv2.cv.CV_32F, dx = 0, dy = 1, ksize = -1)
 
-    #Получаем изображение с высоким значением горизонтального градиента и низким значенм вертикального
+    # Получаем изображение с высоким значением горизонтального градиента и низким значенм вертикального
     gradient = cv2.subtract(gradX, gradY)
     gradient = cv2.convertScaleAbs(gradient)
 
-    #Убираем шумы на изображение
+    # Убираем шумы на изображение
     blurred = cv2.blur(gradient, (1, 1))
-    #Проведем бинаризацию изображения
+    # Проведем бинаризацию изображения
     (_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
 
-    #Превратим слова в белые полоски
+    # Превратим подпись в белые полоски
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
     closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
     closed = cv2.erode(closed, None, iterations = 1)
@@ -218,33 +231,38 @@ def analis_object_in_img(img):
     #Находим контуры на изображение
     (cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    mass = [] #массив найденых слов
+    mass = [] # Массив найденых слов
 
-    #Считаем количество слов
+    # Считаем количество элементов
     i = 0
     square = 0.0
     for element in cnts:
         c = sorted(cnts, key = cv2.contourArea, reverse = True)[i]
-        rect = cv2.minAreaRect(c) #определим минимальный ограничивающий прямоугольник (координаты слов)
-        if(rect[1][0] > 15 and rect[1][0] > 15): #отсеиваем мильчайшие контуры
+        rect = cv2.minAreaRect(c) # Определяем минимальный прямоугольник описывающий элемент
+
+        if(rect[1][0] > 15 and rect[1][0] > 15): # Отсеиваем мельчайшие прямоугольники
             box = np.int0(cv2.cv.BoxPoints(rect))
-            mass += [box] #Добавляем прямоугольник с найденным словом
+            mass += [box] # Добавляем прямоугольник с найденным элементом
             cv2.drawContours(img, [box], -1, (0, 255, 0), 3)
 
+            # Вычислем площадь прямоугольника
             square += math.sqrt(math.pow(box[0][0] - box[1][0], 2) + math.pow(box[0][1] - box[1][1], 2)) * \
                       math.sqrt(math.pow(box[1][0] - box[2][0], 2) + math.pow(box[1][1] - box[2][1], 2))
 
             i += 1
 
+    # Убираем вложенные списки
     mass = [y for x in mass for y in x]
     tmp = mass[:]
 
-    tmp.sort(key=lambda rows: rows[0])  #сортируем массив по оси X
-    mass.sort(key=lambda rows: rows[1]) #сортируем массив по оси Y
+    tmp.sort(key=lambda rows: rows[0])  # Сортируем массив по оси X
+    mass.sort(key=lambda rows: rows[1]) # Сортируем массив по оси Y
 
+    # Определяем минимальный и максимальный координаты прямоугольников
     max = (tmp[-1][0], mass[-1][1])
     min = (tmp[0][0], mass[0][1])
 
+    # Вычисляем длину и ширину подписи
     width  = max[0] - min[0]
     height = max[1] - min[1]
 
@@ -253,6 +271,14 @@ def analis_object_in_img(img):
     return param
 
 def bFMatch(temp, varif):
+    """
+    Brute-Force Matching with SIFT Descriptors and Ratio Test
+
+    @param temp  : Изображение шаблон
+    @param varif : Сверяемое изображение
+
+    @return : Результат сравнения
+    """
 
     img1 = mapping_img(temp)   # queryImage
     img2 = mapping_img(varif)  # trainImage
@@ -309,6 +335,10 @@ verifiable_image = "var2_main3.png"
 template_image   = "test3.png"
 verifiable_image = "test4.png"
 
+template_image   = "template.jpg"
+#verifiable_image = "varifiable.jpg"
+verifiable_image = "varifiable_not_a.jpg"
+
 # Открываем изображение
 image1 = cv2.imread(template_image)
 image2 = cv2.imread(verifiable_image)
@@ -336,7 +366,7 @@ criterion = {}
 # Определяем выполнение критериев
 criterion['count_line'] = "true" if math.fabs(param_template['lines'].size / 2  - param_verifiable['lines'].size / 2)  < 100 else "false"
 criterion['count_circ'] = "true" if math.fabs(param_template['circle'].size / 2 - param_verifiable['circle'].size / 2) < 40  else "false"
-criterion['averadge'] =   "true" if math.fabs(param_template['averadge'] - param_verifiable['averadge']) < 6.0 else "false"
+criterion['averadge'] =   "true" if math.fabs(param_template['averadge'] - param_verifiable['averadge']) < 8.0 else "false"
 criterion['height']   =   "true" if math.fabs(param_template['height']   - param_verifiable['height'])   < 100 else "false"
 criterion['width']   =    "true" if math.fabs(param_template['width']    - param_verifiable['width'])    < 100 else "false"
 criterion['count_elem'] = "true" if math.fabs(param_template['count']    - param_verifiable['count'])    < 2   else "false"
@@ -373,49 +403,7 @@ print u"| Проверка успешна пройден. Подписи сов�
 
 print "+===========================================================================+"
 
-
-#ret = cv2.matchShapes((c1), (c2), 2, 0.0)
-
-# sorted(cnt1)
-# rect = cv.MinAreaRect2(cnt2)
-# i = 0
-# for element in cnt1:
-#     c = sorted(cnt1, reverse = True)[i]
-#     rect = cv.MinAreaRect2(cnt1)
-#     if(rect[1][0] > 15 and rect[1][0] > 15):
-#         #mass.append(rect)
-#         box = np.int0(cv2.cv.BoxPoints(rect))
-#         cv2.drawContours(image1, [box], -1, (0, 255, 0), 3)
-#         i = i+1
-#
-#
-# box = cv.BoxPoints(rect)
-# #box = cv2.minAreaRect(c1[0])
-# box = np.int0(box)
-#
-# #cv2.polylines(image1,[box],True,(255,0,255),2)# draw rectangle in blue color
-# for j in [0, 1 ,2, 3]:
-#     cv.Line( cv.fromarray(image2), grid(box[j]), grid(box[(j+1)%4]), (0,255,255), 20)
-# cv2.drawContours(image1,c1[0],0,(255,0,255),2)
-# cv.DrawContours(cv.fromarray(image2), cnt2, cv.CV_RGB(0,255,255), cv.CV_RGB(0,255,0), 2,2,-1)
-
-
-# epsilon = 0.1*cv2.arcLength(cnt1,True)
-# approx = cv2.approxPolyDP(cnt1,epsilon,True)
-#
-# th3 = cv2.adaptiveThreshold(img1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-# ret = cv2.matchShapes(cnt1, cnt2, 1, 0.0)
-
-# plt.subplot(2,1,1),plt.imshow(thresh2,'gray')
-# plt.title("Azazaz")
-# plt.xticks([]),plt.yticks([])
-#
-# plt.subplot(2,1,2),plt.imshow(th4,'gray')
-# plt.title("Azazaz2")
-# plt.xticks([]),plt.yticks([])
-#
-# plt.show()
-
+# Выводим изображения с выделенными элементами
 cv2.namedWindow("Template", 1)
 cv2.imshow("Template", image1)
 
